79eb120a52975a072902e323ecc74d137dd8586c
franfan <13889441200@163.com>
2019-06-06 13:41:30 +0800

    选排完成

--- Alogrithm/Sort/SelectSort.java
+++ Alogrithm/Sort/SelectSort.java
@@ -5,5 +5,22 @@ public class SelectSort<E extends Comparable<E>> implements Sort<E> {
 		if (list == null) {
 			return;
 		}
+
+		for (int i = 0, size = list.size(); i < size; i++) {
+
+			int idx = i;
+			E min = (E)list.get(i);
+
+			for (int j = i + 1; j < size; j++) {
+				if (min.compareTo(list.get(j)) > 0) {
+					idx = j;
+					min = list.get(j);
+				}
+			}
+
+			E temp = list.get(i);
+			list.set(i, min);
+			list.set(idx, temp);
+		}
 	}
 }
