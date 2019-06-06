65ac90116c8411276f1397e8df84b7b09dce67f6
franfan <13889441200@163.com>
2019-06-06 13:31:30 +0800

    完成插入排序

--- Alogrithm/Sort/InsertSort.java
+++ Alogrithm/Sort/InsertSort.java
@@ -2,5 +2,26 @@ import java.util.List;
 
 public class InsertSort<E extends Comparable<E>> implements Sort<E> {
 	public void sort(List<E> list, boolean desc) {
+		if (list == null) {
+			return;
+		}
+
+		for (int i = 1, size = list.size();  i < size; i++) {
+
+			int j = i - 1;
+
+			E temp = (E)list.get(i);
+
+			if (temp == null || list.get(j) == null) {
+				// 不排序
+				return;
+			}
+
+			for (; j >= 0 && list.get(j).compareTo(temp) > 0; j--) {
+				list.set(j + 1, list.get(j));
+			}
+
+			list.set(j + 1, temp);
+		}
 	}
 }
