3fa8df8200fec1d98d665be245e7a84b5f049b53
franfan <13889441200@163.com>
2019-06-06 14:22:56 +0800

    shell 排序完成

--- Alogrithm/Sort/ShellSort.java
+++ Alogrithm/Sort/ShellSort.java
@@ -5,5 +5,25 @@ public class ShellSort<E extends Comparable<E>> implements Sort<E> {
 		if (list == null) {
 			return;
 		}
+
+		
+		int size = list.size();
+		int step = list.size() / 2;
+
+		while (step > 0) {
+
+			for (int j = 0, i = step; i < size; i += step) {
+
+				E temp = (E)list.get(i);
+
+				for (j = i - step; j >= 0 && temp.compareTo(list.get(j)) < 0; j -= step) {
+					list.set(j + step, list.get(j));
+				}
+
+				list.set(j + step, temp);
+			}
+
+			step /= 2;
+		}
 	}
 }
