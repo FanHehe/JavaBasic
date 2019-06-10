96b73c345bece54816a5a7a23ee3f6e79e6dd4a8
franfan <13889441200@163.com>
2019-06-11 00:01:33 +0800

    完成归并排序

--- Alogrithm/Sort/HeapSort.java
+++ Alogrithm/Sort/HeapSort.java
@@ -6,21 +6,21 @@ public class HeapSort<E extends Comparable<E>> implements Sort<E> {
             return;
         }
 
-        for (int size = list.size(), i = ((size - 1)/ 2); i >= 0; i--) {
-            E curr = list.get(i);
-            E left = list.get(2 * i + 1);
-            E right = list.get(2 * i + 2);
+        // for (int size = list.size(), i = ((size - 1)/ 2); i >= 0; i--) {
+        //     E curr = list.get(i);
+        //     E left = list.get(2 * i + 1);
+        //     E right = list.get(2 * i + 2);
 
-            if (right != null && right.compareTo(list.get(i)) < 0) {
-                list.set(i, right);
-                list.set(2 * i + 2, curr);
-            }
+        //     if (right != null && right.compareTo(list.get(i)) < 0) {
+        //         list.set(i, right);
+        //         list.set(2 * i + 2, curr);
+        //     }
 
-            if (left != null && left.compareTo(list.get(i)) < 0) {
-                E temp = list.get(i);
-                list.set(i, left);
-                list.set(2 * i + 1, temp);
-            }
-        }
+        //     if (left != null && left.compareTo(list.get(i)) < 0) {
+        //         E temp = list.get(i);
+        //         list.set(i, left);
+        //         list.set(2 * i + 1, temp);
+        //     }
+        // }
     }
 }
--- Alogrithm/Sort/MergeSort.java
+++ Alogrithm/Sort/MergeSort.java
@@ -1,9 +1,61 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class MergeSort<E extends Comparable<E>> implements Sort<E> {
     public void sort(List<E> list, boolean desc) {
-        if (list == null) {
+        if (list == null || list.size() < 2) {
             return;
         }
+
+        List<E> result = mergeSort(list, 0, list.size() - 1);
+
+        for (int i = 0, size = list.size(); i < size; i++) {
+            list.set(i, result.get(i));
+        }
+    }
+
+    List<E> mergeSort(List<E> list, int from, int to) {
+        if (to == from) {
+            List<E> array = new ArrayList<>(1);
+            array.add(list.get(from));
+            return array;
+        }
+
+        int middle = (to + from) / 2;
+
+        List<E> left = mergeSort(list, from, middle);
+        List<E> right = mergeSort(list, middle + 1, to);
+
+        return merge(left, right);
+    }
+
+    List<E> merge(List<E> left, List<E> right) {
+
+        int lsize = left.size();
+        int rsize = right.size();
+
+        List<E> array = new ArrayList<>(lsize + rsize);
+
+        int i = 0, j = 0;
+
+        while (i < lsize && j < rsize) {
+            if (left.get(i).compareTo(right.get(j)) > 0) {
+                array.add(right.get(j));
+                j++;
+            } else {
+                array.add(left.get(i));
+                i++;
+            }
+        }
+
+        while (i < lsize) {
+            array.add(left.get(i++));
+        }
+
+        while(j < rsize) {
+            array.add(right.get(j++));
+        }
+
+        return array;
     }
 }
