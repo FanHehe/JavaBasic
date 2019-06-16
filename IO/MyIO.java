d3fe2e28bf7c2ce5b5cb8f0f976f1691d41940a4
franfan <13889441200@163.com>
2019-06-16 17:30:02 +0800

    添加IO相关

--- IO/MyIO.java
+++ IO/MyIO.java
@@ -2,4 +2,16 @@ package IO;
 
 public class MyIO {
 
+    public static void handleBIO() {
+
+    }
+
+    public static void handleNIO() {}
+    public static void handleAIO() {}
+
+    public static void main(String[] args) {
+        handleAIO();
+        handleBIO();
+        handleNIO();
+    }
 }
