52c3d11b83c7ab1af278dadac108e7653a6a2727
franfan <13889441200@163.com>
2019-07-06 09:56:07 +0800

    完成字符串算法

--- Util/Common.java
+++ Util/Common.java
@@ -28,4 +28,16 @@ public interface Common {
 
         println("");
     }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void swap(char[] array, int i, int j) {
+        char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
